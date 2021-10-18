eb8e9c884d1aa6b0acd69a441a85801dc194f539
sergeichestakov <me@sergei.com>
2021-10-18 00:25:00 -0700

    Delete unused components and add profile pic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,29 @@
-import { Hero } from "../components/Hero";
+import { Avatar, Box, Flex, Heading } from "@chakra-ui/react";
 import { Container } from "../components/Container";
-import { Main } from "../components/Main";
-import { DarkModeSwitch } from "../components/DarkModeSwitch";
 
 export default function Index() {
   return (
     <Container height="100vh">
-      <Hero />
-      <Main></Main>
-
-      <DarkModeSwitch />
+      <Flex
+        flexDir="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Flex justify="center" align="center">
+          <Box padding="24px">
+            <Avatar
+              name="Sergei Chestakov"
+              src="/sergeichestakov.jpg"
+              size="xl"
+            />
+          </Box>
+          <Box>
+            <Heading fontSize="8xl">Hi, I'm Sergei</Heading>
+            <Heading fontSize="2xl">Software Engineer & Creator</Heading>
+          </Box>
+        </Flex>
+      </Flex>
     </Container>
   );
 }
