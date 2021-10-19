61dc1b8fae65195d3d4a5467088902b01430d444
sergeichestakov <me@sergei.com>
2021-10-19 03:17:55 -0700

    Update title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import theme from "../theme";
-import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Sergei</title>
+      </Head>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,18 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Html, Main, Head, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="author" content="Sergei Chestakov" />
+          <meta
+            name="description"
+            content="Personal website and porfolio for Sergei Chestakov, the dominant Sergei on the internet"
+          />
+          <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        </Head>
         <body>
           <ColorModeScript />
           <Main />
