20415b465072e9162581063395e4e8652d5c0119
sergeichestakov <me@sergei.com>
2021-10-25 01:26:09 -0700

    Cleaner about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,33 +10,35 @@ export default function About() {
     <Container id="about" pt="32px">
       <Flex flexDir="column" justify="center" align="center" maxWidth="700px">
         <Header href="#about">About</Header>
-        <Box width="100%" paddingBottom="16px">
-          <Text>
-            I'm currently a software engineer at{" "}
-            <Link href="https://replit.com" isExternal color={BLUE}>
-              Replit
-            </Link>{" "}
-            where we're building the future of programming. Previously, I've
-            worked at{" "}
-            <Link href="https://coda.io" isExternal color={BLUE}>
-              Coda
-            </Link>
-            {", "}
-            <Link href="https://duckduckgo.com" isExternal color={BLUE}>
-              Google
-            </Link>
-            {", and "}
-            <Link href="https://expo.io" isExternal color={BLUE}>
-              Expo
-            </Link>
-            . I've also contracted for a number of other startups on the side.
-          </Text>
-        </Box>
-        <Box width="100%">
-          <Text>
-            I'm originally from St. Petersburg, Russia and am now living in San
-            Francisco, CA.
-          </Text>
+        <Box p="5" maxWidth="100%">
+          <Box width="100%" paddingBottom="16px">
+            <Text>
+              I'm currently a software engineer at{" "}
+              <Link href="https://replit.com" isExternal color={BLUE}>
+                Replit
+              </Link>{" "}
+              where we're building the future of programming. Previously, I've
+              worked at{" "}
+              <Link href="https://coda.io" isExternal color={BLUE}>
+                Coda
+              </Link>
+              {", "}
+              <Link href="https://duckduckgo.com" isExternal color={BLUE}>
+                Google
+              </Link>
+              {", and "}
+              <Link href="https://expo.io" isExternal color={BLUE}>
+                Expo
+              </Link>
+              . I've also contracted for a number of other startups on the side.
+            </Text>
+          </Box>
+          <Box width="100%">
+            <Text>
+              I'm originally from St. Petersburg, Russia and am now living in
+              San Francisco, CA.
+            </Text>
+          </Box>
         </Box>
       </Flex>
     </Container>
