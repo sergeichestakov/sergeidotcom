4c24a85dc4ec59bef942a3f903121c4ba4d32149
sergeichestakov <me@sergei.com>
2021-10-25 21:43:41 -0700

    Wrap content in link in Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,34 +9,30 @@ interface Props {
 }
 
 export default function Card({ title, description, image, href }: Props) {
-  const linkRef = React.useRef<HTMLAnchorElement | null>(null);
-
   return (
-    <Box p="5">
-      <Box
-        p="5"
-        width="320px"
-        height="350px"
-        borderWidth="1px"
-        _hover={{ cursor: "pointer", borderColor: "blue.300" }}
-        onClick={() => {
-          linkRef.current.click();
-        }}
-      >
-        <Image
-          borderRadius="md"
-          size="md"
-          width="100%"
-          height="200px"
-          objectFit="contain"
-          src={image}
-        />
-        <Text mt={2} fontSize="18px" fontWeight="semibold" lineHeight="short">
-          {title}
-        </Text>
-        <Text mt={2}>{description}</Text>
-      </Box>
-      <Link href={href} isExternal ref={linkRef} display="none" />
+    <Box p="20px">
+      <Link href={href} isExternal _hover={{ textDecoration: "none" }}>
+        <Box
+          p="20px"
+          width="320px"
+          height="350px"
+          borderWidth="1px"
+          _hover={{ cursor: "pointer", borderColor: "blue.300" }}
+        >
+          <Image
+            borderRadius="md"
+            size="md"
+            width="100%"
+            height="200px"
+            objectFit="contain"
+            src={image}
+          />
+          <Text mt={2} fontSize="18px" fontWeight="semibold" lineHeight="short">
+            {title}
+          </Text>
+          <Text mt={2}>{description}</Text>
+        </Box>
+      </Link>
     </Box>
   );
 }
