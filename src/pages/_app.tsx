a5a01cd1cac32240076432a867cb010193e938b8
sergeichestakov <me@sergei.com>
2021-11-04 21:38:38 -0700

    Update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Sergei</title>
+        <title>Sergei Chestakov | Portfolio</title>
       </Head>
       <ChakraProvider resetCSS theme={theme}>
         <Component {...pageProps} />
