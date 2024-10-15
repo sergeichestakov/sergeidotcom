5d04b3253035cf634eb3cdd6c52679f52df2a360
sergeichestakov <me@sergei.com>
2024-10-15 00:09:14 -0400

    overflow hidden

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const socials = [
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] overflow-hidden items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col">
           <div className="flex gap-4 items-center flex-row">
